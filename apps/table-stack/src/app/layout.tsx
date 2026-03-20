7b79f793f5777edea057a38b769a3b115841e99b
Thomas Wolfe <tomwolfe@gmail.com>
2026-03-20 18:12:19 -0500

    updated

--- apps/table-stack/src/app/layout.tsx
+++ apps/table-stack/src/app/layout.tsx
@@ -28,9 +28,10 @@ export default function RootLayout({
 }>) {
   return (
     <ClerkProvider>
-      <html lang="en">
+      <html lang="en" suppressHydrationWarning>
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+          suppressHydrationWarning
         >
           <NervousSystemProvider autoSubscribe={true}>
             {children}
